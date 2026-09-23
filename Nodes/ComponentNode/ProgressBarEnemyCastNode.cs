e6f0a27c0e1b5813ff1889450c2e4f4fe32b4dc2
AtmoOmen <atmoomen2004@outlook.com>
2026-09-23 23:29:19 +0800

    ProgressBarEnemyCastNode

--- Nodes/ComponentNode/ProgressBarEnemyCastNode.cs
+++ Nodes/ComponentNode/ProgressBarEnemyCastNode.cs
@@ -1,75 +1,150 @@
-﻿using System.Numerics;
+using System.Numerics;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using KamiToolKit.Enums;
 using KamiToolKit.Nodes.Simplified;
+using KamiToolKit.Timelines;
 
 namespace KamiToolKit.Nodes;
 
 /// <summary>
 ///     Specialization of <see cref="ProgressNode" /> to represent the castbar that enemies use.
+///     Modeled off ui/uld/EnemyList.uld and ui/uld/TargetInfoCastBar.uld.
 /// </summary>
-public unsafe class ProgressBarEnemyCastNode : ProgressNode
+public class ProgressBarEnemyCastNode : ProgressNode
 {
+    private const int INTERRUPT_FRAME_LENGTH_LENGTH = 30;
+    private const int INTERRUPT_FRAME_OFFSET        = 15;
+    private const int INTERRUPT_MAX_COUNT           = 2;
+
+    private readonly List<SimpleImageNode> interruptNodes = [];
+
     /// <summary>
     ///     Constructs a new <see cref="ProgressBarEnemyCastNode" />.
     /// </summary>
     public ProgressBarEnemyCastNode()
     {
-        BackgroundImageNode = new SimpleNineGridNode
+        BackgroundNode = new SimpleImageNode
         {
-            TexturePath        = "ui/uld/PartyList_GaugeCast.tex",
-            TextureSize        = new Vector2(204.0f, 20.0f),
-            TextureCoordinates = new Vector2(0.0f,   12.0f),
-            LeftOffset         = 20,
-            RightOffset        = 20
+            TexturePath        = "ui/uld/EnemyList.tex",
+            TextureCoordinates = new Vector2(0.0f,   120.0f),
+            TextureSize        = new Vector2(120.0f, 16.0f)
         };
-        BackgroundImageNode.AttachNode(this);
+        BackgroundNode.AttachNode(this);
 
-        ProgressNode = new SimpleNineGridNode
+        ProgressBarNode = new SimpleImageNode
         {
-            TexturePath        = "ui/uld/PartyList_GaugeCast.tex",
-            TextureSize        = new Vector2(188.0f, 7.0f),
-            TextureCoordinates = new Vector2(8.0f,   3.0f),
-            LeftOffset         = 10,
-            RightOffset        = 10
+            TexturePath        = "ui/uld/EnemyList.tex",
+            TextureCoordinates = new Vector2(0.0f,   104.0f),
+            TextureSize        = new Vector2(120.0f, 16.0f),
+            OriginX            = 4.0f
         };
-        ProgressNode.AttachNode(this);
+        ProgressBarNode.AttachNode(this);
+
+        for (var index = 0; index < INTERRUPT_MAX_COUNT; index++)
+        {
+            var startFrame = 1          + (index                         * INTERRUPT_FRAME_OFFSET);
+            var peakFrame  = startFrame + (INTERRUPT_FRAME_LENGTH_LENGTH / 2);
+            var endFrame   = startFrame + INTERRUPT_FRAME_LENGTH_LENGTH;
+
+            var interruptNode = new SimpleImageNode
+            {
+                TexturePath        = "ui/uld/Interrupt.tex",
+                TextureCoordinates = new Vector2(0.0f,   160.0f),
+                TextureSize        = new Vector2(232.0f, 32.0f),
+                WrapMode           = WrapMode.Stretch,
+                AddColor           = new Vector3(1.0f, 0.0f, -80.0f / 255.0f),
+                Alpha              = 0.0f
+            };
+            interruptNode.AttachNode(this);
+
+            interruptNode.AddTimeline
+            (
+                new TimelineBuilder()
+                    .BeginFrameSet(startFrame, endFrame)
+                    .AddFrame
+                    (
+                        startFrame,
+                        alpha: 0,
+                        scale: new Vector2(1.0f, 1.0f)
+                    )
+                    .AddFrame
+                    (
+                        peakFrame,
+                        alpha: (byte)(255 - (index * 75))
+                    )
+                    .AddFrame
+                    (
+                        endFrame,
+                        alpha: 0,
+                        scale: new Vector2(1.2f, 2f)
+                    )
+                    .EndFrameSet()
+                    .Build()
+            );
+
+            interruptNodes.Add(interruptNode);
+        }
+
+        LoadTimelines();
+
+        Progress = 0.0f;
     }
 
     /// <summary>
     ///     Not intended for public use, but it's here if you absolutely need it.
     /// </summary>
-    public NineGridNode BackgroundImageNode { get; }
+    public SimpleImageNode BackgroundNode { get; }
 
     /// <summary>
     ///     Not intended for public use, but it's here if you absolutely need it.
     /// </summary>
-    public NineGridNode ProgressNode { get; }
+    public SimpleImageNode ProgressBarNode { get; }
 
-    /// <inheritdoc />
-    public override float Progress
+    /// <summary>
+    ///     Not intended for public use, but it's here if you absolutely need it.
+    /// </summary>
+    public IReadOnlyList<SimpleImageNode> InterruptNodes => interruptNodes;
+
+    /// <summary>
+    ///     Gets or sets whether the cast can be interrupted, playing the interrupt animation.
+    /// </summary>
+    public bool IsInterruptible
     {
-        get => ProgressNode.Width / Width;
-        set => ProgressNode.Width = Width * value;
+        get;
+        set
+        {
+            field = value;
+
+            if (value)
+                Timeline?.PlayAnimation(AtkTimelineJumpBehavior.LoopForever, 1);
+            else
+                Timeline?.StopAnimation();
+        }
     }
 
     /// <inheritdoc />
     public override Vector4 BackgroundColor
     {
-        get => new(BackgroundImageNode.AddColor.X, BackgroundImageNode.AddColor.Y, BackgroundImageNode.AddColor.Z, BackgroundImageNode.ResNode->Color.A / 255.0f);
-        set
-        {
-            BackgroundImageNode.ResNode->Color = new Vector4(1.0f, 1.0f, 1.0f, value.W).ToByteColor();
-            BackgroundImageNode.AddColor       = value.AsVector3Color();
-        }
+        get => BackgroundNode.Color;
+        set => BackgroundNode.Color = value;
     }
 
     /// <inheritdoc />
     public override Vector4 BarColor
     {
-        get => new(ProgressNode.AddColor.X, ProgressNode.AddColor.Y, ProgressNode.AddColor.Z, ProgressNode.ResNode->Color.A / 255.0f);
+        get => ProgressBarNode.Color;
+        set => ProgressBarNode.Color = value;
+    }
+
+    /// <inheritdoc />
+    public override float Progress
+    {
+        get;
         set
         {
-            ProgressNode.ResNode->Color = new Vector4(1.0f, 1.0f, 1.0f, value.W).ToByteColor();
-            ProgressNode.AddColor       = value.AsVector3Color();
+            field = Math.Clamp(value, 0.0f, 1.0f);
+
+            ProgressBarNode.ScaleX = field;
         }
     }
 
@@ -78,7 +153,32 @@ public unsafe class ProgressBarEnemyCastNode : ProgressNode
     {
         base.OnSizeChanged();
 
-        BackgroundImageNode.Size = Size;
-        ProgressNode.Size        = Size;
+        BackgroundNode.Position = Vector2.Zero;
+        BackgroundNode.Size     = Size;
+
+        ProgressBarNode.Position = Vector2.Zero;
+        ProgressBarNode.Size     = Size;
+
+        foreach (var interruptNode in interruptNodes)
+        {
+            interruptNode.Position = Vector2.Zero;
+            interruptNode.Size     = new Vector2(120.0f, 16.0f);
+            interruptNode.Origin   = new Vector2(60.0f,  8.0f);
+        }
+    }
+
+    private void LoadTimelines()
+    {
+        const int TOTAL_FRAMES = 1 + (INTERRUPT_FRAME_OFFSET * (INTERRUPT_MAX_COUNT - 1)) + INTERRUPT_FRAME_LENGTH_LENGTH;
+
+        AddTimeline
+        (
+            new TimelineBuilder()
+                .BeginFrameSet(1, TOTAL_FRAMES)
+                .AddLabel(1,            1, AtkTimelineJumpBehavior.Start,       0)
+                .AddLabel(TOTAL_FRAMES, 0, AtkTimelineJumpBehavior.LoopForever, 1)
+                .EndFrameSet()
+                .Build()
+        );
     }
 }
