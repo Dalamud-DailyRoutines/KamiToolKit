b0d0acfbfffb1e3f4d41fc0e311462120fc7b13c
AtmoOmen <atmoomen2004@outlook.com>
2026-09-23 14:25:12 +0800

    reformat

--- Enums/CostTextColor.cs
+++ Enums/CostTextColor.cs
@@ -6,43 +6,49 @@ using KamiToolKit.Nodes;
 namespace KamiToolKit.Enums;
 
 /// <summary>
-/// Enum representing a <see cref="HotbarNode"/>'s cost text color.
+///     Enum representing a <see cref="HotbarNode" />'s cost text color.
 /// </summary>
-public enum CostTextColor {
-
+public enum CostTextColor
+{
     /// <summary>
-    /// Purple-ish Color representing Mana.
+    ///     Purple-ish Color representing Mana.
     /// </summary>
     Mana,
 
     /// <summary>
-    /// Brown-ish Color representing CP.
+    ///     Brown-ish Color representing CP.
     /// </summary>
-    DoL,
+    DoL
 }
 
 /// <summary>
-/// Extensions for <see cref="CostTextColor"/> to get the vector color from the enum value.
+///     Extensions for <see cref="CostTextColor" /> to get the vector color from the enum value.
 /// </summary>
-public static class CostTextColorExtensions {
-    extension(CostTextColor color) {
-
+public static class CostTextColorExtensions
+{
+    extension
+    (
+        CostTextColor color
+    )
+    {
         /// <summary>
-        /// Gets the RGBA primary text color for this value.
+        ///     Gets the RGBA primary text color for this value.
         /// </summary>
-        public Vector4 TextColor => color switch {
+        public Vector4 TextColor => color switch
+        {
             CostTextColor.Mana => new Vector4(1.000f, 0.851f, 0.980f, 1.000f), // Slight Purple
-            CostTextColor.DoL => new Vector4(1.000f, 0.945f, 0.831f, 1.000f), // Slight brown
-            _ => KnownColor.White.Vector(),
+            CostTextColor.DoL  => new Vector4(1.000f, 0.945f, 0.831f, 1.000f), // Slight brown
+            _                  => KnownColor.White.Vector()
         };
 
         /// <summary>
-        /// Gets the RGBA outline color for this value.
+        ///     Gets the RGBA outline color for this value.
         /// </summary>
-        public Vector4 TextOutlineColor => color switch {
+        public Vector4 TextOutlineColor => color switch
+        {
             CostTextColor.Mana => new Vector4(0.596f, 0.314f, 0.565f, 1.000f),
-            CostTextColor.DoL => new Vector4(0.498f, 0.486f, 0.114f, 1.000f),
-            _ => KnownColor.Black.Vector(),
+            CostTextColor.DoL  => new Vector4(0.498f, 0.486f, 0.114f, 1.000f),
+            _                  => KnownColor.Black.Vector()
         };
     }
 }
